4438686b7b18f274a551d0e7a4219fa6ae2a4ece
Yesmine Boukrayaa <bkyesmina053@gmail.com>
2026-02-16 21:01:17 +0100

    fix: résolution conflit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print('Bienvenue sur notre application collective ! Bon travail à l\'équipe !')
 print('Fonctionnalité de login ajoutée par Etudiant A.')
 print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
->>>>>>> main
+ main
 
 print('Hello, world! Bienvenue à tous les collaborateurs !')
 print('Fonctionnalité de login ajoutée par Etudiant A.')
 print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
->>>>>>> main
+ main
