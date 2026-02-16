13d10a6a0f25b4685173623c7e40dec6f9cb2a19
Yesmine Boukrayaa <bkyesmina053@gmail.com>
2026-02-16 13:05:22 +0100

    style: Mise à jour du message d'accueil et du titre

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-print('Bienvenue dans notre application collaborative !')
-print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
+print('Bienvenue sur notre application collective ! Bon travail à l\'équipe !')
 print('Fonctionnalité de login ajoutée par Etudiant A.')
+print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
 >>>>>>> main
 
